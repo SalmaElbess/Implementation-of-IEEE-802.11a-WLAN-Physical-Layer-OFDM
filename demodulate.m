220772f3afd0742cb343b11a4067bf468cc4b30e
Elsayed Mostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2021-05-24 12:13:01 +0200

    modulation and demodulation implemented

---
+++ demodulate.m
@@ -0,0 +1,36 @@
+function output_stream = demodulate(input_stream, mod_type, additional, output_type)
+%% demodulate: This function perform demodulation for input complex symbols with multiple modulation schemes
+% Parameters: 
+    % input_stream: modulated complex symbols
+    % mod_type: the needed modulation schemce (N-QAM, PSK, QPSK)
+    % additional: additional information depending on mod_type. In case of
+    % QAM, it's the needed reference vector for demodulation
+    % output_type: the type of the output stream (symbols, binary)
+    
+% Returns:
+    % output_stream: the output stream (binary or symbols).
+%% ---------------------------------------------------------------- %%
+if strcmpi(mod_type, '16qam')
+  % In case of 16-QAM , the additional is the ref_demodualtor
+  [~,I] = min(conj(input_stream') - additional,[], 2);
+  demod_samples = (I-1)';
+elseif strcmpi(mod_type, '64qam')
+  % In case of 64-QAM , the additional is the ref_demodualtor
+  [~,I] = min(conj(input_stream') - additional,[], 2);
+  demod_samples = (I-1)';
+  elseif strcmpi(mod_type, 'qpsk')
+  % In case of QPSK , the additional is the ref_demodualtor
+  [~,I] = min(conj(input_stream') - additional,[], 2);
+  demod_samples = (I-1)';
+elseif strcmpi(mod_type, 'bpsk')
+  demod_samples = zeros(size(input_stream));
+  demod_samples(input_stream > 0 ) = 1;
+end
+  output_stream = demod_samples;
+ 
+% If the needed output format is binary, convert to binary data.
+ if strcmpi(output_type, 'binary')
+   output_stream = de2bi(output_stream, 'left-msb');
+   output_stream = reshape(output_stream', 1, []); 
+ end
+end
---
+++ modulate.m
@@ -0,0 +1,54 @@
+function [mapped_stream, additional] = modulate(input_stream, input_type, mod_type, coding_scheme_vec)
+%% modulate: This function perform modulation for input data (binary or symbols) with multiple modulation schemes
+% Parameters: 
+    % input_stream: a row vector of the input data
+    % input_type: the type of input_stream 'binary' or 'symbols'
+    % mod_type: the needed modulation schemce (N-QAM, PSK, QPSK)
+% Returns:
+    % mapped_stream: the output modulated complex symbols
+    % additional: additional information depending on mod_type. In case of
+    % QAM, it's the needed reference vector for demodulation
+%% ---------------------------------------------------------------- %%
+ % Get the number of symbols for mod_type
+ if strcmpi(mod_type, 'bpsk')
+    N = 2;
+ elseif strcmpi(mod_type,'qpsk')
+    N = 4;
+ elseif strcmpi(mod_type,'16qam')
+    N = 16;
+ elseif strcmpi(mod_type,'64qam')
+    N = 64;
+ end
+
+ % Binary data conversion 
+if strcmpi(input_type,'binary')
+    % --- Zero Padding for correct dimensions
+    if rem(length(input_stream), log2(N)) ~= 0
+        input_stream =  padarray(input_stream, [0, log2(N)-ceil(rem(length(input_stream),log2(N)))], 0, 'post');
+    end
+    binary_data = reshape(input_stream, log2(N), [])';
+    stream = bi2de(binary_data, 'left-msb')';
+else
+    stream = input_stream;
+end
+
+%  Modulation
+additional = 0; Kmod = 1; %initialization
+ %-- check if coding scheme is passed
+if nargin < 4
+   coding_scheme_vec = -1; %default value in case of no specific coding scheme 
+end
+
+   if strcmpi(mod_type, '16qam') || strcmpi(mod_type, '64qam') || strcmpi(mod_type, 'qpsk')
+      Kmod = 1/sqrt(10); %IEEE-11.802a standard
+      [mapped_stream, additional] = modulate_QAM(N, stream, coding_scheme_vec);
+      mapped_stream = mapped_stream * Kmod;
+   elseif strcmpi(mod_type, 'bpsk')
+      mapped_stream = stream; 
+      mapped_stream(mapped_stream == 0) = -1;
+   else
+       disp('Invalid modulation type');
+       return
+   end
+   additional = additional * Kmod;
+end
