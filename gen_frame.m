ef1a9a3aa4d6edba4d0f27c71b93246ae791c8d0
ElsayedMMostafa <sayedmmostaf255@gmail.com>
2021-05-30 14:06:22 +0200

    finalized

--- gen_frame.m
+++ gen_frame.m
@@ -1,4 +1,4 @@
-clear; clc;
+clear; clc; close all;
 N = 64; guard_len = 16; % OFDM parameters
 rate= 1/2; mod_type = '16qam';
 
@@ -6,34 +6,36 @@ zero_indecies = cat(2, 1, (28:38));   %indecies of zeroes
 pilots_indecies = [44, 57, 8, 22];
 data_indecies = setdiff(setdiff((1:64), pilots_indecies), zero_indecies);
 
-input_length = 1013;
+input_length = 10000;
 bin_data = randi([0,1], [1,input_length]);
 [temp_preamble, temp_signal, temp_data] = construct_frame(bin_data, mod_type, rate);
 
 %% Preamble & Signal post-processing
-temp_preamble = reshape(temp_preamble,[],4)'; 
+temp_preamble = reshape(temp_preamble,[],4).'; 
 preamble = zeros(4,64); signal = zeros(1,64);
 
 rest_indecies = setdiff((1:64), zero_indecies);
 for i=1:4
-    preamble(rest_indecies) = temp_preamble(i,:);
+    preamble(i,rest_indecies) = temp_preamble(i,:);
 end
-preamble = reshape(preamble',1,[]);
+preamble = reshape(preamble.',1,[]);
 signal(rest_indecies) = temp_signal;
 
 %% Data post-processing
 n_data = length(data_indecies);
 % -- padding
 data = padarray(temp_data, [0, n_data-ceil(rem(length(temp_data),n_data))], 0, 'post');
-tx_pilots = data(randperm(n_data, length(pilots_indecies)));
+tx_pilots = temp_data(randperm(20, length(pilots_indecies))); % 20 is totally a random number
 % -- add pilots
-data = conj(reshape(data,n_data,[])');
+data = reshape(data,n_data,[]).';
 final_data = zeros(size(data,1), 64);
 for r=1:size(data,1)
     final_data(r,pilots_indecies) = tx_pilots;
     final_data(r,data_indecies) = data(r,:);
 end
- final_data = reshape(conj(final_data'), 1, []);
+ final_data = reshape(final_data.', 1, []);
+ % Constellation
+ %scatterplot(final_data);
  % FINALLY, concatenate with the preamble and signal
  frame = [preamble, signal, final_data];  % READY for transmission
 %% OFDM  
@@ -47,7 +49,9 @@ with_cyclic_guards = [after_ifft(N-guard_len+1:end,:);after_ifft];
 with_cyclic_guards = reshape(with_cyclic_guards,1,[]);
 
 %% TODO#1: Channel  
-  % ---
+h = [0.8208 + 0.2052*1i, 0.4104 + 0.1026*1i, 0.2052 + 0.2052*1i, 0.1026 + 0.1026*1i];
+%with_cyclic_guards = conv(with_cyclic_guards,h);
+%with_cyclic_guards = with_cyclic_guards(1:end-length(h)+1);
  
 %% Rx Side
   % Given we have (with_cyclic_guards) as the input after channel equalization
@@ -63,20 +67,41 @@ rec_frame = reshape(after_fft,1,[]);  %it should be the same as (frame)
 rec_preamble = rec_frame(1:64*4);
 rec_signal = rec_frame(64*4+1:64*5);
 rec_data = rec_frame(64*5+1:end);     %it should be the same as (final_data)
-rec_data = conj(reshape(rec_data, 64, [])');
+rec_data = reshape(rec_data, 64, []).';
 % For each data symbol, extract (pilots, real_data)
  rx_data = [];
+ rx_pilots = [];
 for i=1:size(rec_data,1)
-   rx_pilot = rec_data(i, pilots_indecies); %% SHOULD BE CONCATENATED ...
+   rx_pilots = cat(1, rx_pilots, rec_data(i, pilots_indecies)); %% SHOULD BE CONCATENATED ...
    rx_data =  cat(2,rx_data, rec_data(i, data_indecies));  % multiple of 48 now
 end
 %% TODO#2: Channel Estimation
   % ---
+  %channel_gains = estimate_channel(rec_preamble,'ZF');
+  %Constructing ture preamble values
+   L = [1 1 -1 -1 1 1 -1 1 -1 1 1 1 1 1 1 -1 -1 1 1 -1 1 -1 1 1 1 1 1 -1 -1 1 1 -1 1 -1 1 -1 -1 -1 -1 -1 1 1 -1 -1 1 -1 1 -1 1 1 1 1];
+   %zero_indecies = cat(2, 1, (28:38));   %indecies of zeroes
+   %rest_indecies = setdiff((1:64), zero_indecies);
+   %get the long recieved preamble symbols
+   long_rec_preamble_1 = rec_preamble(rest_indecies + 64*2);
+   long_rec_preamble_2 = rec_preamble(rest_indecies + 64*3);
+   h1 = long_rec_preamble_1./L;
+   h2 = long_rec_preamble_2./L;
+   channel_gains = (h1+h2)./2;
 %% TODO#3: Channel Equalization
   % ---
-
+    rx_pilots_indecies = [44-12, 57-12, 8-1, 22-1];
+    rx_data_indecies = setdiff((1:52), rx_pilots_indecies);
+    data_channel_gains = channel_gains(rx_data_indecies);
+    equalized_data = zeros(size(rx_data));
+    for i = 1:48:length(rx_data)
+        equalized_data(i:i+47) = rx_data(i:i+47)./data_channel_gains;
+    end
+ 
+% Costellation
+ scatterplot(equalized_data);
 % Symbol Demapping
- out_coded = demodulate(rx_data, mod_type, 'binary');
+ out_coded = demodulate(equalized_data, mod_type, 'binary');
  %% TODO#4: Extract data_length , rate from signal
 % given that I know the rate and data length 
  rx_length = input_length; rx_rate = rate;%must be extracted from signal
--- main_scenario.m
+++ main_scenario.m
@@ -3,6 +3,7 @@ clear; clc; close all;
 %% Main Scenario script
 Nc = 64; guard_len = 16; % OFDM parameters
 rate= 1/2; mod_type = '16qam';
+h = [0.8208 + 0.2052*1i, 0.4104 + 0.1026*1i, 0.2052 + 0.2052*1i, 0.1026 + 0.1026*1i]; %channel
 
 FileID=fopen('test_file_1.txt','r');                       %open the file in read mode                                                                  
 %Reading the file
@@ -13,24 +14,22 @@ end
 fclose(FileID);
 data = reshape(dec2bin(data, 8).'-'0',1,[]);
 
-%% Transmitter
+%% The main loop
 out_decoded=[];
 for i=1:8000:length(data)
     frame = data(i:min(length(data),i+7999)); 
+    % Tansmitter
     tx_frame = WiFi_transmitter(frame, mod_type, rate, Nc, guard_len);
-
-
     % Channel
-    h = [0.8208 + 0.2052*1i, 0.4104 + 0.1026*1i, 0.2052 + 0.2052*1i, 0.1026 + 0.1026*1i];
     Rx_frame = conv(tx_frame,conj(h));
     Rx_frame = Rx_frame(1:end-length(h)+1);
     % Receiver
     out_decoded = [out_decoded WiFi_receiver(Rx_frame, Nc, guard_len)];
 end
 %% Check
-BER = sum(out_decoded ~= data)/length(out_decoded);
-disp(BER);
+BER = sum(out_decoded ~= data)/length(out_decoded)
+
 FileID=fopen('rec_test_file_1.txt','w');
 fprintf(FileID,'%c',char(bin2dec(reshape(char(out_decoded+'0'), 8,[]).'))');
-fclose(FileID)
+fclose(FileID);
 
